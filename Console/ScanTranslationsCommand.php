29e06f4f6dd50619f403df1d3d240a5afa8b4265
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2023-07-12 23:53:23 +0300

    Update ScanTranslationsCommand.php

--- Console/ScanTranslationsCommand.php
+++ Console/ScanTranslationsCommand.php
@@ -11,7 +11,7 @@ class ScanTranslationsCommand extends Command
 {
     protected $signature = 'translations:scan {directory : Path to the folder}';
 
-    protected $description = 'Scanning files and extracting trans() function keys and values';
+    protected $description = 'Scanning files and extracting trans(), __(), @lang() function keys and values';
 
     public function handle(): void
     {
@@ -23,26 +23,60 @@ class ScanTranslationsCommand extends Command
             RecursiveIteratorIterator::LEAVES_ONLY
         );
 
-        $pattern = '/trans\([\'"]([^\'"]+)[\'"],\s*\[[^\]]*\'default\'\s*=>\s*([\'"][^\'"]+[\'"])\]/';
+        $defaultPattern = '/(?<!\w)(trans|__|@lang)\([\'"]([^\'"]*)[\'"],\s*\[[^\]]*\'default\'\s*=>\s*([\'"][^\'"]*[\'"])\]/';
+        $allKeysPattern = '/(?<!\w)(trans|__|@lang)\([\'"]([^\'"]*)[\'"](,\s*\[[^]]+\])?\)/';
+
 
         foreach ($iterator as $file) {
             if ($file->isFile() && $file->getExtension() === 'php') {
                 $content = file_get_contents($file->getPathname());
 
                 $matches = [];
-                preg_match_all($pattern, $content, $matches, PREG_SET_ORDER);
-
+                preg_match_all($defaultPattern, $content, $matches, PREG_SET_ORDER);
                 foreach ($matches as $match) {
-                    $key = $match[1];
-                    $value = $match[2];
+                    $key = $match[2];
+                    $value = $match[3];
+
                     $translations[$key] = $value;
+                }
+
+                $matches = [];
+                preg_match_all($allKeysPattern, $content, $matches, PREG_SET_ORDER);
+                foreach ($matches as $match) {
+                    $key = $match[2];
+                    if (!isset($translations[$key])) {
+                        $translations[$key] = $key;
+                    }
                 }
             }
         }
 
-        $this->warn('Знайдені ключі та значення функції trans() __() @trans():');
+        $this->warn('Found keys and values of functions trans(), __(), @lang():');
+        $this->info("");
+        $locFile = '';
+        $oldModule = '';
+        $oldLocFile = '';
         foreach ($translations as $key => $value) {
-            $this->info( "'{$key}' => {$value},");
+
+            if (str_contains($key, "::")) {
+                [$module, $key] = explode('::', $key, 2);
+                [$locFile, $key] = explode('.', $key, 2);
+                if ($module != $oldModule){
+                    $oldModule = $module;
+                    $this->warn("Localizations module: $module");
+                }
+            } elseif (str_contains($key, ".")) {
+                [$locFile, $key] = explode('.', $key, 2);
+            }
+
+            if ($locFile != $oldLocFile){
+                $oldLocFile = $locFile;
+                $this->warn("Localizations path: $locFile.php");
+            }
+
+
+
+            $this->info("'$key' => $value,");
         }
     }
 }
